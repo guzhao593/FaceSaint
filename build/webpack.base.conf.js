9c2b846f13a11e8fbe4016789103e8d221479bd0
guzhao593 <guzhao593@163.com>
2018-07-14 11:17:50 +0800

    配置webpack

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,8 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'pages': resolve('src/pages'),
+      'components': resolve('src/components')
     }
   },
   module: {
---
+++ src/pages/home/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>222222244442311111111111111111113112</div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {}
+  }
+}
+</script>
