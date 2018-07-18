fe080905d3a0515c9c4b6a72e16f6c6a7c21658f
guzhao593 <guzhao593@163.com>
2018-07-18 21:16:12 +0800

    添加fastclick

--- src/main.js
+++ src/main.js
@@ -3,12 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import FastClick from 'fastclick'
 import 'style/index.scss'
 import '@/assets/iconfont/iconfont.css'
 import '@/assets/iconfont/iconfont.js'
 Vue.config.productionTip = false
-
+// 添加fastclick,解决移动端浏览器点击延迟300ms的问题
+if ('addEventListener' in document) {
+  // dom加载完成后再初始化实例
+  document.addEventListener('DOMContentLoaded', function () {
+    // 初始化实例
+    FastClick.attach(document.body)
+  }, false)
+}
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
